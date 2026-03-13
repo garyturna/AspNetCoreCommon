6148e3034ed21e96c55e81aefb4e0f8aca636305
Gary Turner <garyturna@hotmail.com>
2026-03-13 15:04:19 +1300

    Data Access Classes/Interfaces added

---
+++ DataLibrary/Data/FoodData.cs
@@ -0,0 +1,27 @@
+﻿using DataLibrary.Db;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Data
+{
+    public class FoodData : IFoodData
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly ConnectionStringData _connectionString;
+
+        public FoodData(IDataAccess dataAccess, ConnectionStringData connectionString)
+        {
+            _dataAccess = dataAccess;
+            _connectionString = connectionString;
+        }
+
+        public Task<List<FoodModel>> GetFood()
+        {
+            return _dataAccess.LoadData<FoodModel>("dbo.spFood_GetAll",
+                                                    new { },
+                                                    _connectionString.SqlConnectionString);
+        }
+    }
+}
---
+++ DataLibrary/Data/OrderData.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using DataLibrary.Db;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DataLibrary.Data
+{
+    public class OrderData : IOrderData
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly ConnectionStringData _connectionString;
+
+        public OrderData(IDataAccess dataAccess, ConnectionStringData connectionString)
+        {
+            _dataAccess = dataAccess;
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> CreateOrder(OrderModel order)
+        {
+            DynamicParameters p = new DynamicParameters();
+            p.Add("@OrderName", order.OrderName);
+            p.Add("@OrderDate", order.OrderDate);
+            p.Add("@FoodId", order.FoodId);
+            p.Add("@Quantity", order.Quantity);
+            p.Add("@Total", order.Total);
+            p.Add("@Id", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            await _dataAccess.SaveData("dbo.spOrders_Insert", p, _connectionString.SqlConnectionString);
+
+            return p.Get<int>("@Id");
+        }
+
+        public Task<int> UpdateOrderName(int orderId, string orderName)
+        {
+            return _dataAccess.SaveData("dbo.spOrders_UpdateOrderName",
+                                        new { Id = orderId, OrderName = orderName },
+                                        _connectionString.SqlConnectionString);
+        }
+
+        public Task<int> DeleteOrder(int orderId)
+        {
+            return _dataAccess.SaveData("dbo.spOrders_Delete",
+                                        new { Id = orderId },
+                                        _connectionString.SqlConnectionString);
+        }
+
+        public async Task<OrderModel> GetOrderById(int orderId)
+        {
+            var recs = await _dataAccess.LoadData<OrderModel>("dbo.spOrders_GetById",
+                                                        new { Id = orderId },
+                                                        _connectionString.SqlConnectionString);
+            return recs.FirstOrDefault();
+        }
+
+    }
+}
