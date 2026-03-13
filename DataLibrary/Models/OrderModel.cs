985a705b138aae70412cd4a2ed73d521914b857d
Gary Turner <garyturna@hotmail.com>
2026-03-13 15:02:41 +1300

    Food and Order Models created

---
+++ DataLibrary/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+        public DateTime OrderDate{ get; set; }
+        public int FoodId{ get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
