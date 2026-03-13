54c8683ee819c5dc6e04316643e25f6a611b65aa
Gary Turner <garyturna@hotmail.com>
2026-03-13 15:03:36 +1300

    Database helper classes/interfaces

---
+++ DataLibrary/Db/ConnectionStringData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Db
+{
+    public class ConnectionStringData
+    {
+        public string SqlConnectionString { get; set; } = "Default";
+    }
+}
---
+++ DataLibrary/Db/IDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DataLibrary.Db
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T>(string storedProcedure, object parameters, string connectionStringName);
+        Task<int> SaveData(string storedProcedure, object parameters, string connectionStringName);
+    }
+}
