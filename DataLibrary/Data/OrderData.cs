41a39e7b2540df5166d719658c10532e408a90c9
Gary Turner <garyturna@hotmail.com>
2026-03-17 14:35:59 +1300

    Updated Stored procedure name from spOrders_UpdateOrderName to spOrders_UpdateName

--- DataLibrary/Data/OrderData.cs
+++ DataLibrary/Data/OrderData.cs
@@ -36,7 +36,7 @@ namespace DataLibrary.Data
 
         public Task<int> UpdateOrderName(int orderId, string orderName)
         {
-            return _dataAccess.SaveData("dbo.spOrders_UpdateOrderName",
+            return _dataAccess.SaveData("dbo.spOrders_UpdateName",
                                         new { Id = orderId, OrderName = orderName },
                                         _connectionString.SqlConnectionString);
         }
